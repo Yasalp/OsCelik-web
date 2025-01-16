fd86b1fc2cc74fd40290823d0fccb4c0298ef19b
Soner Demirci <soner.demirci10@gmail.com>
2025-01-16 15:39:26 +0300

    General editing

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home-page/HomePage";
 import Contact from "./pages/contact/Contact";
-import Menu from "./components/menu/Menu";
 import Project from "./pages/project/Project";
+import Navbar from "./components/navbar/Navbar";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/project" element={<Project />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </Router>
   );
 };
